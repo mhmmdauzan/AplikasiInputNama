d8fbc9450043aa65e3122c9c967d5071f49460b0
mhmmdauzan <mhmmd.auzan4123@gmail.com>
2020-01-26 03:01:38 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/auzan/aplikasiinputnama/MainActivity.java
@@ -0,0 +1,29 @@
+package id.ac.poliban.mi.auzan.aplikasiinputnama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText etNama = findViewById(R.id.etNama);
+        Button btShow = findViewById(R.id.btShow);
+
+        btShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Selamat Pagi " + etNama.getText().toString() + " Semoga Hari Anda Menyenangkan", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
